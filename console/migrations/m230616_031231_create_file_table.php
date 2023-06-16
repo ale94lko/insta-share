f8bc94db2de8897fbcca5b67ce130b27dfdbf37d
Fidel Alejandro Fernández Arias <47539231+ale94lko@users.noreply.github.com>
2023-06-15 23:41:41 -0400

    Add create_file_table migration

---
+++ console/migrations/m230616_031231_create_file_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%file}}`.
+ */
+class m230616_031231_create_file_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%file}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(100)->notNull(),
+            'path' => $this->string(255)->notNull(),
+            'status' => $this->string(20)->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'created_by' => $this->integer(),
+        ]);
+
+        $this->addForeignKey('FK_file_user_created_by', '{{%file}}', 'created_by', '{{%user}}', 'id');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey('FK_file_user_created_by', '{{%file}}');
+        $this->dropTable('{{%file}}');
+    }
+}
