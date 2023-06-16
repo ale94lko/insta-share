1337c5958fec6730d8d525df2e7b396f4998207f
Fidel Alejandro Fernández Arias <47539231+ale94lko@users.noreply.github.com>
2023-06-16 02:19:02 -0400

    Checks the privilege of the current user to his own files

--- backend/controllers/FileController.php
+++ backend/controllers/FileController.php
@@ -3,8 +3,10 @@
 namespace backend\controllers;
 
 use backend\resources\File;
+use Yii;
 use yii\filters\auth\HttpBearerAuth;
 use yii\rest\ActiveController;
+use yii\web\ForbiddenHttpException;
 
 class FileController extends ActiveController
 {
@@ -18,5 +20,20 @@ class FileController extends ActiveController
         ];
 
         return $behaviors;
+    }
+
+    /**
+     * Checks the privilege of the current user to his own files.
+     *
+     * @param string $action the ID of the action to be executed
+     * @param File $model the model to be accessed. If null, it means no specific model is being accessed.
+     * @param array $params additional parameters
+     * @throws ForbiddenHttpException if the user does not have access
+     */
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        if ($model->created_by !== Yii::$app->user->id) {
+            throw new ForbiddenHttpException('You do not have access to this file');
+        }
     }
 }
